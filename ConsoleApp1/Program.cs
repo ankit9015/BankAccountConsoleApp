f872f6e7932ade805955d519a4f2f16e5162b266
Ankit <ankit@smarttask.io>
2023-05-07 15:17:07 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            // Test that the initial balances must be positive.
+        /*    BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+                return;
+            }*/
+
+
+            // Test for a negative balance.
+        /*    try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }*/
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            Console.ReadLine();
+        }
+
+        static int Cube(int num)
+        {
+            int result = num * num;
+            return result;
+        }
+    }
+}
